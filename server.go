7ea88b6bb55e25e7e0affdaab281552e69178de3
Aidahar <zagirik@gmail.com>
2021-03-27 21:23:59 +0300

    Fix server and add items and list

--- server.go
+++ server.go
@@ -21,6 +21,6 @@ func (s *Server) Run(port string, handler http.Handler) error {
 	return s.httpServer.ListenAndServe()
 }
 
-func (s *Server) Shutdown(ctx context.Context) {
+func (s *Server) Shutdown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
 }
